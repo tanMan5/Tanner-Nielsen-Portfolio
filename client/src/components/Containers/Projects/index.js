135fe6c8978b32509225f19c43e085b4b2e42279
Tanner Nielsen <tan-ali5@outlook.com>
2020-10-26 14:36:10 -0700

    each card is set with github and deployed

--- client/src/components/Containers/Projects/index.js
+++ client/src/components/Containers/Projects/index.js
@@ -9,7 +9,7 @@ function ProjectContainer() {
                <hr />
                <article class="row mt-4">
                    <div class="col-md-6 col-lg-6 d-flex justify-content-center images">
-                   <img class="img-thumbnail projectOne" src="./assets/images/Quizzical.png" alt="Quizzical" />
+                   <img class="img-thumbnail projectOne" src="client\src\components\img\Quizzical.png" alt="Quizzical" />
                    <div class="hoverOver">
                        <h3>Project 1: Quizzical</h3>
                        <a target="blank" href="https://tanman5.github.io/07-project-01/" class="project">
@@ -24,75 +24,75 @@ function ProjectContainer() {
                    </div>
 
                    <div class="col-md-6 col-lg-6 d-flex justify-content-center images">
-                   <img class="img-thumbnail projectOne" src="./assets/images/Quizzical.png" alt="Quizzical" />
+                   <img class="img-thumbnail projectTwo" src="./assets/images/Quizzical.png" alt="Book-Tracker" />
                    <div class="hoverOver">
                        <h3>Project 2: Book Tracker</h3>
-                       <a target="blank" href="https://tanman5.github.io/07-project-01/" class="project">
+                       <a target="blank" href="https://book-trackerapp.herokuapp.com/" class="project">
                            <h4>Deployed</h4>
                        </a>
                        <>
                        </>
-                       <a target="blank" href="https://github.com/tanman5/07-project-01" class="project">
+                       <a target="blank" href="https://github.com/tanMan5/Library-App" class="project">
                        <h4>GitHub Repository</h4>
                        </a>
                    </div>
                    </div>
 
                    <div class="col-md-6 col-lg-6 d-flex justify-content-center images">
-                   <img class="img-thumbnail projectOne" src="./assets/images/Quizzical.png" alt="Quizzical" />
+                   <img class="img-thumbnail projectThree" src="./assets/images/Quizzical.png" alt="Ground Up" />
                    <div class="hoverOver">
                        <h3>Project 3: GroundUp</h3>
-                       <a target="blank" href="https://tanman5.github.io/07-project-01/" class="project">
+                       <a target="blank" href="https://infinite-coast-64919.herokuapp.com/" class="project">
                            <h4>Deployed</h4>
                        </a>
                        <>
                        </>
-                       <a target="blank" href="https://github.com/tanman5/07-project-01" class="project">
+                       <a target="blank" href="https://github.com/tanMan5/project3-groundup" class="project">
                        <h4>GitHub Repository</h4>
                        </a>
                    </div>
                    </div>
 
                    <div class="col-md-6 col-lg-6 d-flex justify-content-center images">
-                   <img class="img-thumbnail projectOne" src="./assets/images/Quizzical.png" alt="Quizzical" />
+                   <img class="img-thumbnail hw" src="./assets/images/Quizzical.png" alt="Eat Da Burger" />
                    <div class="hoverOver">
                        <h3>HW: Eat Da Burger</h3>
-                       <a target="blank" href="https://tanman5.github.io/07-project-01/" class="project">
+                       <a target="blank" href="https://limitless-badlands-47193.herokuapp.com/" class="hw">
                            <h4>Deployed</h4>
                        </a>
                        <>
                        </>
-                       <a target="blank" href="https://github.com/tanman5/07-project-01" class="project">
+                       <a target="blank" href="https://github.com/tanMan5/HW13.Burger" class="hw">
                        <h4>GitHub Repository</h4>
                        </a>
                    </div>
                    </div>
 
                    <div class="col-md-6 col-lg-6 d-flex justify-content-center images">
-                   <img class="img-thumbnail projectOne" src="./assets/images/Quizzical.png" alt="Quizzical" />
+                   <img class="img-thumbnail hw" src="./assets/images/Quizzical.png" alt="Password Generator" />
                    <div class="hoverOver">
                        <h3>HW: Password-Generator</h3>
-                       <a target="blank" href="https://tanman5.github.io/07-project-01/" class="project">
+                       <a target="blank" href="https://tanman5.github.io/Password-Generator/" class="hw">
                            <h4>Deployed</h4>
                        </a>
                        <>
                        </>
-                       <a target="blank" href="https://github.com/tanman5/07-project-01" class="project">
+                       <a target="blank" href="https://github.com/tanMan5/Password-Generator" class="hw">
                        <h4>GitHub Repository</h4>
                        </a>
                    </div>
                    </div>
 
                    <div class="col-md-6 col-lg-6 d-flex justify-content-center images">
-                   <img class="img-thumbnail projectOne" src="./assets/images/Quizzical.png" alt="Quizzical" />
+                   <img class="img-thumbnail hw" src="./assets/images/Quizzical.png" alt="Employee Directory" />
                    <div class="hoverOver">
                        <h3>HW: Employee Directory</h3>
-                       <a target="blank" href="https://tanman5.github.io/07-project-01/" class="project">
+                       <a target="blank" href="https://tanman5.github.io/HW19.employeesDirectory/" class="hw">
                            <h4>Deployed</h4>
                        </a>
                        <>
                        </>
-                       <a target="blank" href="https://github.com/tanman5/07-project-01" class="project">
+                       <a target="blank" href="https://github.com/tanMan5/HW19.employeesDirectory" class="hw">
                        <h4>GitHub Repository</h4>
                        </a>
                    </div>

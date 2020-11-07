53ee3f97c2a479af44f6f409c86bf593f72de474
Tanner Nielsen <tan-ali5@outlook.com>
2020-11-06 21:50:51 -0800

    added font awesome for styling

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
+import "./style.css"
 
 
 class Footer extends Component {
@@ -9,11 +10,19 @@ class Footer extends Component {
         return (
             <Container>
                 <Navbar sticky="bottom" expand="lg" variant="light" bg="light">
-                    <Nav className="container navbar justify-content-end flex-column flex-sm-row" activeKey="/home">
-                        Made by
-
-
-                    </Nav>
+                    <Nav className="container navbar justify-content flex-column flex-sm-row" activeKey="/home">
+                        Made by Tanner Nielsen
+                   
+                    <div className="social-links">
+                    <a href="https://www.linkedin.com/in/tanernielsen/" rel="noopener noreferrer" target="_blank">
+                        <i className="fa fa-linkedin-square" aria-hidden="true" />
+                    </a>
+                    <a href="https://github.com/tanMan5" rel="noopener noreferrer" target="_blank">
+                        <i className="fa fa-github fa-7x" aria-hidden="true" />
+                    </a>
+                </div>
+                </Nav>
+          
                 </Navbar>
             </Container>
         )

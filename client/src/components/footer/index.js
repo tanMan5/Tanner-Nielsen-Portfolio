48108f1ba2fcb1d06122df10efc8c0e63547a4f3
Tanner Nielsen <tan-ali5@outlook.com>
2020-11-09 10:31:46 -0800

    added font awesome/styled linkedin icon

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -15,7 +15,7 @@ class Footer extends Component {
                    
                     <div className="social-links">
                     <a href="https://www.linkedin.com/in/tanernielsen/" rel="noopener noreferrer" target="_blank">
-                        <i className="fa fa-linkedin-square" aria-hidden="true" />
+                        <i className="fa fa-linkedin-square fa-7x" aria-hidden="true" />
                     </a>
                     <a href="https://github.com/tanMan5" rel="noopener noreferrer" target="_blank">
                         <i className="fa fa-github fa-7x" aria-hidden="true" />
